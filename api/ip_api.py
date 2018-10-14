89c02960205fbb2a58b824460b496766bb39bb3a
kerimov <kerimov225@mail.ru>
2018-10-14 17:03:39 +0300

    Refactoring

--- api/ip_api.py
+++ api/ip_api.py
@@ -9,8 +9,8 @@ def get_data():
     return response.json()
 
 
-def check_ip(ip):
-    numbers = [int(n) for n in ip.split(".")]
+def check_ip(ip_address):
+    numbers = [int(n) for n in ip_address.split(".")]
     for number in numbers:
         if number <= 0 or number > 255:
             return False
